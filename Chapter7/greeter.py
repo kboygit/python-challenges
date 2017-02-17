2b2f1903ece6928b30128104ca03f8c75d0e45be
kboygit <heybisayangdako@gmail.com>
2017-02-17 12:05:29 -0800

    initialize Chapter7

---
+++ Chapter7/greeter.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
+
+prompt = "\nIf you tell us who you are, we can personalize"
+prompt += "\nWhat is your first name?"
+
+name = input(prompt)
+print("\nHello " + name + "!")
